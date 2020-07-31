335298bdf6889285c6b3b4d5ea62491693f9d7f0
Hardik Batra <67378572+hardikbatra17@users.noreply.github.com>
2020-07-31 23:52:03 +0530

    Add files via upload

---
+++ plot3.R
@@ -0,0 +1,19 @@
+file <- "./data/household_power_consumption.txt"
+power <- read.table(file, header = T, sep = ";", na.strings = "?", 
+                    stringsAsFactors = F)
+subSetData <- power[power$Date %in% c("1/2/2007","2/2/2007") ,]
+
+datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+
+globalActivePower <- as.numeric(subSetData$Global_active_power)
+subMetering1 <- as.numeric(subSetData$Sub_metering_1)
+subMetering2 <- as.numeric(subSetData$Sub_metering_2)
+subMetering3 <- as.numeric(subSetData$Sub_metering_3)
+
+png("plot3.png", width=480, height=480)
+plot(datetime, subMetering1, type="l", ylab="Energy Submetering", xlab="")
+lines(datetime, subMetering2, type="l", col="red")
+lines(datetime, subMetering3, type="l", col="blue")
+legend("topright", legend= c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
+       col = c("black", "red", "blue"), lty = 1, lwd = 2)
+dev.off()
---
+++ plot4.R
@@ -0,0 +1,31 @@
+file <- "./data/household_power_consumption.txt"
+power <- read.table(file, header = T, sep = ";", na.strings = "?", 
+                    stringsAsFactors = F)
+subSetData <- power[power$Date %in% c("1/2/2007","2/2/2007") ,]
+
+datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+
+globalActivePower <- as.numeric(subSetData$Global_active_power)
+globalReactivePower <- as.numeric(subSetData$Global_reactive_power)
+voltage <- as.numeric(subSetData$Voltage)
+subMetering1 <- as.numeric(subSetData$Sub_metering_1)
+subMetering2 <- as.numeric(subSetData$Sub_metering_2)
+subMetering3 <- as.numeric(subSetData$Sub_metering_3)
+
+png("plot4.png", width=480, height=480)
+par(mfrow = c(2, 2)) 
+
+## plot(1,1)
+plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power")
+##plot(1,2)
+plot(datetime, voltage, type="l", xlab="datetime", ylab="Voltage")
+##plot(2,1)
+plot(datetime, subMetering1, type="l", ylab="Energy Submetering", xlab="")
+lines(datetime, subMetering2, type="l", col="red")
+lines(datetime, subMetering3, type="l", col="blue")
+legend("topright", legend= c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
+       col = c("black", "red", "blue"), lty = 1, lwd = 2)
+##plot(2,2)
+plot(datetime, globalReactivePower, type = "l", xlab = "datetime", 
+     ylab = "Global_reactive_power")
+dev.off()
